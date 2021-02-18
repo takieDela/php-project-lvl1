43cb779ba822951f5f8c654c844ccbb23f8ac22b
Sofya <sofya-good@mail.ru>
2021-02-19 01:23:46 +0300

    add return statement to function

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,10 @@ namespace Php\Project\Lvl1\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting()
+function greeting(): bool
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return;
+    return true;
 }
