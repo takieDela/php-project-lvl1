9d0a2ebfe3f19a38814b022f187f02ee1e36a717
Sofya <sofya-good@mail.ru>
2021-02-19 01:34:55 +0300

    add typehint specified

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace Php\Project\Lvl1\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function startGame($description, $gameData)
+function startGame(string $description, array $gameData): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
