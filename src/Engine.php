fb0030dae4aced1a332018531e62cf4695715559
Sofya <sofya-good@mail.ru>
2021-02-19 00:59:09 +0300

    add game engine

---
+++ src/Engine.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Php\Project\Lvl1\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function startGame($description, $gameData)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($description);
+
+    foreach ($gameData as [$question, $rightAnswer]) {
+        line("Question: {$question}");
+        $answer = prompt('Your answer');
+
+        if ($answer == $rightAnswer) {
+            line('Correct!');
+        } else {
+            line("'{$answer} is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+            line("Let's try again, {$name}!");
+            return;
+        }
+    }
+
+    line("Congratulations, {$name}!");
+    return;
+}
