ed1984e5135106d4ad53a3dc6cfcd835947c5c2a
Sofya <sofya-good@mail.ru>
2021-02-19 01:01:16 +0300

    add game logic

---
+++ src/Games/Gcd.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Php\Project\Lvl1\Games\Gcd;
+
+use function Php\Project\Lvl1\Engine\startGame;
+
+use const Php\Project\Lvl1\GameConfig\GAMES_TO_WIN;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function findGcd($number1, $number2)
+{
+    $gcd = 1;
+    $min = min($number1, $number2);
+
+    for ($i = 1; $i <= $min; $i++) {
+        if ($number1 % $i == 0 && $number2 % $i == 0) {
+            $gcd = $i;
+        }
+    }
+
+    return $gcd;
+}
+
+function generateGameData()
+{
+    $gameData = [];
+
+    for ($i = 0; $i < GAMES_TO_WIN; $i++) {
+        $number1 = rand(1, 100);
+        $number2 = rand(1, 100);
+        $rightAnswer = findGcd($number1, $number2);
+
+        $question = "{$number1} {$number2}";
+        $gameData[] = [$question, $rightAnswer];
+    }
+
+    return $gameData;
+}
+
+function gcdGame()
+{
+    $gameData = generateGameData();
+    startGame(DESCRIPTION, $gameData);
+}
