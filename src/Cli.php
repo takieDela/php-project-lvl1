81e41287ceb59c0ef55958aa99b70caae8387bd7
Sofya <sofya-good@mail.ru>
2021-02-19 01:28:26 +0300

    change return typehint specified

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,10 @@ namespace Php\Project\Lvl1\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(): bool
+function greeting(): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return true;
+    return;
 }
