3a4211cee358ac457ce8f3578407f63e4d60d503
Sofya <sofya-good@mail.ru>
2021-02-19 01:01:44 +0300

    add game logic

---
+++ src/Games/Progression.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Php\Project\Lvl1\Games\Progression;
+
+use function Php\Project\Lvl1\Engine\startGame;
+
+use const Php\Project\Lvl1\GameConfig\GAMES_TO_WIN;
+
+const DESCRIPTION = 'What number is missing in the progression?';
+
+function makeProgression($a1, $d, $progressionLength)
+{
+    $progression = [];
+    for ($i = 0; $i < $progressionLength; $i++) {
+        $progression[] = $a1 + $i * $d;
+    }
+
+    return $progression;
+}
+
+function generateGameData()
+{
+    $gameData = [];
+
+    for ($i = 0; $i < GAMES_TO_WIN; $i++) {
+        $a1 = rand(1, 100);
+        $d = rand(1, 10);
+        $progressionLength = rand(5, 10);
+        $progression = makeProgression($a1, $d, $progressionLength);
+        $hidePosition = rand(0, $progressionLength - 1);
+
+        $rightAnswer = $progression[$hidePosition];
+        $progression[$hidePosition] = '..';
+
+        $question = implode(' ', $progression);
+        $gameData[] = [$question, $rightAnswer];
+    }
+
+    return $gameData;
+}
+
+function progressionGame()
+{
+    $gameData = generateGameData();
+    startGame(DESCRIPTION, $gameData);
+}
